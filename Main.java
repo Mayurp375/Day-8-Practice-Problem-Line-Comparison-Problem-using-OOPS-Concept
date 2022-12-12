5fbc2f8db182d2a06db53fe5db0eaafa35cae211
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-12 15:11:55 +0530

    CalculateLengh

---
+++ Main.java
@@ -0,0 +1,9 @@
+package classProblemByAmolMateSir.Day8PracticeProblem.LineComparison;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("line Comparision");
+        CalculateLengh.check();// calculate lengh
+
+    }
+}
