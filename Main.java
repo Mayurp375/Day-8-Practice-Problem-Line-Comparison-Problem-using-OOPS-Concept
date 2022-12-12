ef73be3638b7324737dc09b7e2a0043a0f339e75
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-12 15:14:23 +0530

    line Equality

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("line Comparision");
         CalculateLengh.check();// calculate lengh
-
+        Eqlality.check(); // line Equality
     }
 }
